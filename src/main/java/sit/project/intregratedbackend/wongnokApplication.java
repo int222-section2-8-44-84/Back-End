359191447605729b7b619d7268d1952792d3ae85
WarisaraKhrua <waan.khrua123@gmail.com>
2021-09-05 13:23:27 +0700

    firstCommit

---
+++ src/main/java/sit/project/intregratedbackend/wongnokApplication.java
@@ -0,0 +1,13 @@
+package sit.project.intregratedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class wongnokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(wongnokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sit/project/intregratedbackend/IntregratedbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.project.intregratedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntregratedbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
