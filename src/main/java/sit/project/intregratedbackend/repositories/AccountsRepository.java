fd2d7f856be736c2f9665e3b6d49d68bc5f2ed73
warisara <warisara.khrua@mail.kmutt.ac.th>
2021-09-09 11:57:31 +0700

    [Create]Repository

---
+++ src/main/java/sit/project/intregratedbackend/repositories/AccountsRepository.java
@@ -0,0 +1,7 @@
+package sit.project.intregratedbackend.repositories;
+import sit.project.intregratedbackend.models.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountsRepository extends JpaRepository<Accounts, Long>{
+	
+}
---
+++ src/main/java/sit/project/intregratedbackend/repositories/CategoriesRepository.java
@@ -0,0 +1,8 @@
+package sit.project.intregratedbackend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sit.project.intregratedbackend.models.Categories;
+
+public interface CategoriesRepository extends JpaRepository<Categories, Long>{
+
+}
